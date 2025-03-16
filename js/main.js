457c3be3cdb6b6ada4cdac45e5be7280cfd4de38
Dan Singer <d_singer@fanshaweonline.ca>
2025-03-15 22:49:04 -0400

    Puzzle Functionality
    -Coverted JS files to modules
    -Added a "next puzzle" button
    -Added a category area
    -Ensured all relevant tiles on the board turn white and hide a letter
    -Ensured clicking a letter turns a tile blue
    -Ensured clicking a tile reveals the tile behind
    -Ensured new puzzles get loaded and old puzzles get cleared
    -Minor styling changes

--- js/main.js
+++ js/main.js
@@ -1,50 +1,5 @@
-const arrow = document.querySelector("#arrow")
-const nameSubmit = document.querySelectorAll(".name-submit")
-const moneySubmit = document.querySelectorAll(".money-submit")
+import { functionality } from "./modules/main-functions.js";
+import { puzzleFunctionality } from "./modules/puzzle-functionality.js";
 
-function expandWheelCon() {
-    const wheelCon = document.querySelector("#wheel-con")
-    const wheel = document.querySelector("#wheel")
-
-    if (wheelCon.style.right === "0px") {
-        arrow.textContent = "◄"
-        wheel.style.display = "none"
-        wheelCon.style.right = "-1180px"
-    } else {
-        arrow.textContent = "►"
-        wheel.style.display = "block"
-        wheelCon.style.right = "0px"
-    }
-}
-
-function nameChange() {
-    const parentCheck = this.parentNode 
-    const nameValueCheck = parentCheck.querySelector(".name-value")
-    const nameValue = nameValueCheck.value
-    const dataCheck = parentCheck.dataset.player
-    const nameTextCheck = document.querySelector(`#player-${dataCheck}`)
-    const name = nameTextCheck.querySelector(".player-name")
-
-    name.textContent = nameValue
-    nameValueCheck.value = ""
-}
-
-function changeMoney() {
-    const parentCheck = this.parentNode 
-    const moneyValueCheck = parentCheck.querySelector(".money-value")
-    const moneyValue = moneyValueCheck.value
-    const dataCheck = parentCheck.dataset.player
-    const moneyTextCheck = document.querySelector(`#player-${dataCheck}`)
-    const moneySelect = moneyTextCheck.querySelector(".money-amount")
-    const money = moneySelect.textContent
-    const newValue = parseFloat(moneyValue)
-    const oldValue = parseFloat(money)
-    const newAmount = newValue + oldValue
-
-    moneySelect.textContent = newAmount
-    moneyValueCheck.value = ""
-}
-
-arrow.addEventListener("click", expandWheelCon)
-nameSubmit.forEach(button => button.addEventListener("click", nameChange))
-moneySubmit.forEach(button => button.addEventListener("click", changeMoney))
+functionality()
+puzzleFunctionality()
